357f7f533ffa00a99e5095d8cb5b888cd5227010
Shawn Shipton <sigrud.ds@gmail.com>
2022-07-18 02:23:03 -0700

    Added noip and lets encrypt

--- Client/Client.gd
+++ Client/Client.gd
@@ -1,27 +1,34 @@
 extends Node
 
-var ip:String = "192.168.1.230"
-var port:int = 8888
+var url:String = "godot.hopto.org"
+#var url:String = "www.stopclock.app"
+#var url:String = "stopclock.app"
+var port:int = 9999
+#var port:int = 42092
 var tcp_peer:StreamPeerTCP
 var ssl_peer:StreamPeerSSL
 
 
 func _ready() -> void:
 	tcp_peer = StreamPeerTCP.new()
-	var err : int = tcp_peer.connect_to_host(ip, port)
-	if err :
+	var err:int = tcp_peer.connect_to_host(url, port)
+	if err:
 		print_debug("tcp connection error:", err)
+	else:
+		print("tcp connected")
 		
 	ssl_peer = StreamPeerSSL.new()
 	err = ssl_peer.connect_to_stream(tcp_peer) #will fetch cert from server
-#	err = ssl_peer.connect_to_stream(tcp_peer, true, "your_server_url") #will fetch cert from server and check the url validity
-	if err :
+#	var cert:X509Certificate = load("res://x509/cert1.crt")
+#	err = ssl_peer.connect_to_stream(tcp_peer, true, url, cert) #check the url validity
+	if err:
 		print_debug("ssl connection error:", err)
-	
-	var status = ssl_peer.get_status()
-	if status == StreamPeerSSL.STATUS_CONNECTED :
-		print("connected")
 	else:
-		print("connection failed")
+		var status = ssl_peer.get_status()
+		if status == StreamPeerSSL.STATUS_CONNECTED:
+			print("connected")
+		else:
+			print("connection failed")
+	
 	
 
